4a1475ccda93d6156d53f448949ea73a51d3480e
lauren-m-johnson <laurenmj90@gmail.com>
2023-10-21 11:33:45 -0700

    update for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={'/north_state_junk/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
