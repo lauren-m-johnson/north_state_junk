8539d2f355eb05f3773b392765ed9ac1dc4248d6
lauren-m-johnson <laurenmj90@gmail.com>
2023-10-22 16:53:18 -0700

    add about sectiton to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,16 @@ export default function Home() {
                     <h1>Junk Removal in Sonoma, Mendocino and Lake Counties</h1>
                 </div>
             </section>
+            <section id='about'>
+                <h2>About North State Junk Removal</h2>
+                <p>
+                    Here at North State Junk Removal we offer premier services to our clients and community. Our mission is to provide
+                    professional, affordable and convenient service with an emphasis on customer satisfaction. While junk removal may
+                    be a large part of our business we also strive to improve the North State through donating, recycling and reusing
+                    whatever comes our way. We are always looking forward to a better tomorrow, let us help you get there as we work 
+                    toward a cleaner, greener future for everyone!
+                </p>
+            </section>
         </div>
     )
 }
