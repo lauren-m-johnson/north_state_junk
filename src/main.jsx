4c1ecb2657218527bb5fcb502398a291069903ca
lauren-m-johnson <laurenmj90@gmail.com>
2023-10-22 15:46:51 -0700

    edit browser router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={'/north_state_junk/'}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
