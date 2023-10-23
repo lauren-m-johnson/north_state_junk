272b616ba5a04c179ce198b06dc6e19948e2d584
lauren-m-johnson <laurenmj90@gmail.com>
2023-10-22 18:37:15 -0700

    add list of services to services page

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -2,12 +2,43 @@ import './Services.css';
 
 export default function Services() {
     return (
-        <div className='main-container'>
-                <section id='serv-intro'>
-                <div className='section-text'>
-                    <h1>Services</h1>
-                </div>
-            </section>
-        </div>
+            <div className='main-container'>
+                    <section id='serv-intro'>
+                    <div className='section-text'>
+                        <h1>Services</h1>
+                    </div>
+                </section>
+                <section id='services-list'>
+                    <h2>Services include but are not limited to:</h2>
+                    <div id='list-container'>
+                        <ul>
+                            <li>Residential Junk Removal</li>
+                            <li>Commercial Junk Removal</li>
+                            <li>Construction Debris Removal</li>
+                            <li>Appliance Disposal</li>
+                            <li>Furniture Removal</li>
+                            <li>Yard Waste Removal</li>
+                            <li>Electronics Recycling</li>
+                            <li>Hoarding Cleanouts</li>
+                            <li>Hot Tub Removal</li>
+                            <li>Foreclosure Cleanouts</li>
+                            <li>Estate Cleanouts</li>
+                        </ul>
+                        <ul>
+                            <li>Office Cleanouts</li>
+                            <li>Warehouse Cleanouts</li>
+                            <li>Mattress Disposal</li>
+                            <li>Tire Removal</li>
+                            <li>Carpet Removal</li>
+                            <li>Scrap Metal Recycling</li>
+                            <li>Storage Unit Cleanouts</li>
+                            <li>Garage Cleanouts</li>
+                            <li>Basement Cleanouts</li>
+                            <li>Attic Cleanouts</li>
+                            <li>Property Management Cleanouts</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
     )
 }
