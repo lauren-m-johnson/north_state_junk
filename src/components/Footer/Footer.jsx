a81ae4b3034617aa70f13ec408bc39f5f5aab327
lauren-m-johnson <laurenmj90@gmail.com>
2023-10-23 11:13:39 -0700

    add phone image to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,7 @@ export default function Footer() {
             <Link to='/contact'>
                 <button id='footer-btn'>Contact Us</button>
             </Link>
+            <a href="tel:7072348583"><img src='/images/smartphone.png'></img></a>
             <p>Call or Text: <a href="tel:7072348583">(707)234-8583</a></p>
             <p>Email: <a href="mailto:northstatejunkremoval@gmail.com">northstatejunkremoval@gmail.com</a></p>
         </div>
