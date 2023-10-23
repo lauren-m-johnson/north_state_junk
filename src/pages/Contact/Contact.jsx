dc571b39eb7a6d3ad585db6843253656d5c70a80
lauren-m-johnson <laurenmj90@gmail.com>
2023-10-22 19:08:29 -0700

    update footer

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -8,6 +8,11 @@ export default function Contact() {
                     <h1>Contact</h1>
                 </div>
             </section>
+            <section id='contact-info'>
+                <h2>Call or text us today for a free quote!</h2>
+                {/* <h3>Already recieved services?</h3>
+                <a href=''><h3>Leave us a review on Google!</h3></a> */}
+            </section>
         </div>
     )
 }
